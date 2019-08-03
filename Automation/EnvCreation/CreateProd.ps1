b00d2d51d33e3ed3d4f35039a4878a2f62a00375
Adam Anderson <anderson.adam@gmail.com>
2019-08-03 14:44:36 -0700

    Updated scripts to create env and test if working

---
+++ Automation/EnvCreation/CreateProd.ps1
@@ -0,0 +1,28 @@
+$Prod = "aw_server"
+$HostName = $Prod
+$result = docker start $HostName
+if ($result -ne $HostName) {
+    "Pres:\Helper.psm1" | Import-Module
+    Write-Host "Starting $HostName" -BackgroundColor Blue
+    $mycred = Get-PSCredential -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key -User sa
+    $password = $mycred.GetNetworkCredential().Password
+
+    Write-Host "Create Production Container" -ForegroundColor Green
+    docker run -d `
+        -p 1402:1433 `
+        --name $HostName `
+        --hostname $HostName `
+        -e sa_password=$Password `
+        -e ACCEPT_EULA=Y `
+        -v C:/data/backup/:C:/backup/ `
+        -v C:/data/Scripts/:C:/scripts/ `
+        -v C:/docker/Adv/data/:C:/data/ `
+        -e attach_dbs="[{'dbName':'AdventureWorks','dbFiles':['C:\\data\\AdventureWorks2017.mdf','C:\\data\\AdventureWorks2017_log.ldf']}]" `
+        microsoft/mssql-server-windows-developer
+}
+
+Write-Progress -Activity "Waiting 30 seconds for server to start" -Status "Sleeping"  
+Start-Sleep -Seconds 30
+$configPath = ".\$HostName.DbcConfig.json"
+Import-DbcConfig -Path $configPath
+Invoke-DbcCheck -Tag InstanceConnection -SqlInstance $HostName -SqlCredential $mycred
---
+++ Automation/EnvCreation/CreateQA.ps1
@@ -0,0 +1,40 @@
+# $Prod = "aw_server"
+$EnvName = "aw_qa"
+$result = docker start $EnvName
+if ($result -ne $EnvName) {
+    "Pres:\Helper.psm1" | Import-Module
+    Write-Host "Starting $EnvName" -BackgroundColor Blue
+    $mycred = Get-PSCredential -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key -User sa
+    $password = $mycred.GetNetworkCredential().Password
+
+    Write-Host "Create Preproduction Container" -ForegroundColor Green
+    docker run -d `
+        -p 1410:1433 `
+        --name $EnvName `
+        --hostname $EnvName `
+        -e sa_password=$Password `
+        -e ACCEPT_EULA=Y `
+        -v C:/data/backup/:C:/backup/ `
+        -v C:/data/Scripts/:C:/scripts/ `
+        microsoft/mssql-server-windows-developer
+
+    Write-Host "There is no external Data directory mapped to this container" -ForegroundColor Green
+    Write-Host "Create a Data directory inside this container" -ForegroundColor Green
+    docker exec -it $EnvName powershell mkdir data
+
+    Write-Progress -Activity "Waiting 20 seconds for server to start" -Status "Sleeping"  
+    Start-Sleep -Seconds 20
+    Write-Host "Restore AdventureWorks from Production" -ForegroundColor Green
+    Restore-DbaDatabase `
+        -SqlInstance $EnvName `
+        -SqlCredential $mycred `
+        -Path c:\backup\AdventureWorks.bak `
+        -DatabaseName AdventureWorks `
+        -DestinationDataDirectory c:\data\ `
+        -DestinationLogDirectory c:\data
+
+    Invoke-Sqlcmd -ServerInstance "localhost,1410" -Database master -Username sa -Password $password -InputFile C:\data\Scripts\cmd.sql
+
+    Docker stop $EnvName
+    Docker commit $EnvName "$EnvName.image:demo.automation"
+}
