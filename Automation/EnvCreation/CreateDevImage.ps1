8eb7bd259778b024b1ae2e89150cba052177fa21
Adam Anderson <anderson.adam@gmail.com>
2019-08-07 06:00:05 -0700

    script cleanup

--- Automation/EnvCreation/CreateDevImage.ps1
+++ Automation/EnvCreation/CreateDevImage.ps1
@@ -1,15 +1,16 @@
 # $Prod = "aw_server"
 $EnvName = "aw_dev"
+
+"Pres:\Helper.psm1" | Import-Module
+Write-Host "Starting $EnvName" -BackgroundColor Blue
+$mycred = Get-PSCredential -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key -User sa
+$password = $mycred.GetNetworkCredential().Password
+
 $result = docker start $EnvName
 if ($result -ne $EnvName) {
-    "Pres:\Helper.psm1" | Import-Module
-    Write-Host "Starting $EnvName" -BackgroundColor Blue
-    $mycred = Get-PSCredential -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key -User sa
-    $password = $mycred.GetNetworkCredential().Password
-
-    Write-Host "Create Preproduction Container" -ForegroundColor Green
+    Write-Host "Create $Envname Container" -ForegroundColor Green
     docker run -d `
-        -p 1410:1433 `
+        -p 1420:1433 `
         --name $EnvName `
         --hostname $EnvName `
         -e sa_password=$Password `
@@ -33,11 +34,14 @@ if ($result -ne $EnvName) {
         -DestinationDataDirectory c:\data\ `
         -DestinationLogDirectory c:\data
 
-    Invoke-Sqlcmd -ServerInstance "localhost,1410" -Database master -Username sa -Password $password -InputFile C:\data\Scripts\cmd.sql
-    Invoke-Sqlcmd -ServerInstance "qa11" -Database master -Username sa -Password $password -InputFile "C:\data\Scripts\FixSidOwner.sql"
-    Invoke-Sqlcmd -ServerInstance "qa11" -Database master -Username sa -Password $password -InputFile "C:\data\Scripts\SetClrEnabled.sql"
-    Invoke-Sqlcmd -ServerInstance "qa11" -Database master -Username sa -Password $password -InputFile "C:\data\Scripts\tSQLt.class.sql"
-            
+    Invoke-Sqlcmd -ServerInstance $EnvName -Database master -Username sa -Password $password -InputFile C:\data\Scripts\cmd.sql
+    Invoke-Sqlcmd -ServerInstance $EnvName -Database AdventureWorks -Username sa -Password $password -InputFile "C:\data\Scripts\FixSidOwner.sql"
+    Invoke-Sqlcmd -ServerInstance $EnvName -Database AdventureWorks -Username sa -Password $password -InputFile "C:\data\Scripts\SetClrEnabled.sql"
+    Invoke-Sqlcmd -ServerInstance $EnvName -Database AdventureWorks -Username sa -Password $password -InputFile "C:\data\Scripts\tSQLt.class.sql"
+    
+    Invoke-Sqlcmd -ServerInstance $EnvName -Database AdventureWorks -Username sa -Password $password -InputFile "C:\data\Scripts\RunAllTests.sql" | Select-Object -ExpandProperty xml* | out-file c:\data\results.xml
+#    $result[0] | out-file C:\temp\results.xml
+
     Docker stop $EnvName
     Docker commit $EnvName "$EnvName.image:demo.automation"
 }
