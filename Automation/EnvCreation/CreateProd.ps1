74755d33c2954d028f177e5a3e0f01fa5ccd59f3
Adam Anderson <anderson.adam@gmail.com>
2019-10-06 12:15:57 -0700

    Update script to create better flow for demo

--- Automation/EnvCreation/CreateProd.ps1
+++ Automation/EnvCreation/CreateProd.ps1
@@ -1,22 +1,25 @@
-$Prod = "aw_server"
-$HostName = $Prod
+
+$HostName = "aw_Server"
 $result = docker start $HostName
 if ($result -ne $HostName) {
     "Pres:\Helper.psm1" | Import-Module
     Write-Host "Starting $HostName" -BackgroundColor Blue
-    $mycred = Get-PSCredential -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key -User sa
+    Write-Host "Read password from secure file" -ForegroundColor Green
+    $pwd = Get-SecurePassword -PwdFile Data:\MyPwd.txt -KeyFile Data:\MyKey.key
+    Write-Host "Build the PSCredential object for setting passwords" -ForegroundColor Green
+    $mycred = New-Object System.Management.Automation.PSCredential("sa",$pwd)
     $password = $mycred.GetNetworkCredential().Password
 
     Write-Host "Create Production Container" -ForegroundColor Green
     docker run -d `
         -p 1402:1433 `
         --name $HostName `
-        --hostname $HostName `
         -e sa_password=$Password `
         -e ACCEPT_EULA=Y `
+        -v C:/docker/Adv/data/:C:/data/ `
         -v C:/data/backup/:C:/backup/ `
         -v C:/data/Scripts/:C:/scripts/ `
-        -v C:/docker/Adv/data/:C:/data/ `
+        --hostname $HostName `
         -e attach_dbs="[{'dbName':'AdventureWorks','dbFiles':['C:\\data\\AdventureWorks2017.mdf','C:\\data\\AdventureWorks2017_log.ldf']}]" `
         microsoft/mssql-server-windows-developer
 }
@@ -26,4 +29,3 @@ Start-Sleep -Seconds 30
 $configPath = ".\$HostName.DbcConfig.json"
 Import-DbcConfig -Path $configPath
 Invoke-DbcCheck -Tag InstanceConnection -SqlInstance $HostName -SqlCredential $mycred
-Invoke-DbcCheck -SqlInstance aw_server -Tags DatabaseExists -Database AdventureWorks -SqlCredential $mycred
